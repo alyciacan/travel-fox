2c51cdfe5a46675b662e6a97e5d2dacccd896ac9
Alycia Canavan <alycia.canavan@gmail.com>
2022-09-22 22:24:39 -0700

    Add initial tests on Trip-test.js, some code to pass tests on trip.js.

---
+++ src/Trip.js
@@ -0,0 +1,20 @@
+class Trip {
+  constructor(user, destination, tripObj) {
+    this.id = Date.now(),
+    this.userID = user.id,
+    this.destinationID = destination.id,
+    this.travelers = tripObj.travelers,
+    this.date = tripObj.startDate,
+    this.duration = tripObj.duration,
+    this.status = 'pending',
+    this.suggestedActivities = [];
+  };
+  calculateCost(destinationObj) {
+    const costWithoutFee = (this.travelers * destinationObj.estimatedFlightCostPerPerson)
+     + (this.duration * destinationObj.estimatedLodgingCostPerDay);
+    const fee = costWithoutFee * .1;
+    return `$${(costWithoutFee + fee).toLocaleString()}`;
+  };
+};
+
+module.exports = Trip;
